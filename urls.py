f3475d467966090d58a7ba9371780a09416c5c29
saadiya19wh1a12a4 <69639140+saadiya19wh1a12a4@users.noreply.github.com>
2023-06-12 19:50:32 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('', home, name='home'),
+    path('homeq/', homeq, name='homeq'),
+    path('donorreg/', donor_register, name='donorReg'),
+    path('ngoreg/', ngo_registration, name='ngoReg'),
+    path('donorlog/', donor_login, name='donorLog'),
+    path('ngolog/', ngo_login, name='ngolog'),
+    path('upload/', upload_med, name='uploadMed'),
+    path('view/', view_med, name='viewMed'),
+    path('logout/', logout_user, name='logout'),
+    path('aboutus/', about_us, name='aboutus'),
+    path('contactus/', contact_us, name='contactus'),
+    path('view/<name>', checkout, name='checkout')
+]
